de67cef3355b879f782403da868498da6dc07785
aaartorias <rajputshubhendu@gmail.com>
2022-08-25 16:46:01 +0530

    initialize project

---
+++ src/main/java/com/example/todoApp/TodoApplication.java
@@ -0,0 +1,36 @@
+package main.java.com.example.todoApp;
+
+public class TodoApplication {
+    public static void main(String[] args) {
+    }
+    private void addTask() {
+
+    }
+
+    private void getTask() {
+
+    }
+
+    private void modifyTask() {
+        // making the assumption here about the flow:
+        // we have already fetched the task and made modifications to the task using setters.
+        // in modifyTask(), we are asking to save it in the todo-list and the in-memory data store
+
+    }
+
+    private void removeTask(Integer taskId) {
+
+    }
+
+    private void listTasks() {
+
+    }
+
+    private void getStatistics() {
+
+    }
+
+    private void getActivityLog() {
+
+    }
+}
